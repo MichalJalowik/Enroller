eb208fe2f99d081775fba25ad69eb5baebb6a98d
MJ <michaljalowik@gmail.com>
2020-04-19 12:36:32 +0200

    changed cone in meetingService - no rest controller

--- src/main/java/com/company/enroller/controllers/MeetingRestController.java
+++ src/main/java/com/company/enroller/controllers/MeetingRestController.java
@@ -10,9 +10,12 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Collection;
 
+@RestController
+@RequestMapping("/meetings")
 public class MeetingRestController {
 
     @Autowired
