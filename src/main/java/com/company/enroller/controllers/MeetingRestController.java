bcf81a8488707d263ab96dce802a89db73ccf598
MJ <michaljalowik@gmail.com>
2020-04-19 12:11:46 +0200

    adder meetngREST class

--- src/main/java/com/company/enroller/controllers/MeetingRestController.java
+++ src/main/java/com/company/enroller/controllers/MeetingRestController.java
@@ -1,4 +1,36 @@
 package com.company.enroller.controllers;
 
+import com.company.enroller.model.Participant;
+import com.company.enroller.persistence.ParticipantService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.Collection;
+
 public class MeetingRestController {
+
+    @Autowired
+    ParticipantService meetingService;
+
+    @RequestMapping(value = "", method = RequestMethod.GET)
+    public ResponseEntity<?> getParticipants() {
+        Collection<Participant> participants = meetingService.getAll();
+        return new ResponseEntity<Collection<Participant>>(participants, HttpStatus.OK);
+    }
+
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    public ResponseEntity<?> getMeeting(@PathVariable("id") String login) {
+        Participant participant = meetingService.findByLogin(login);
+        if (participant == null) {
+            return new ResponseEntity(HttpStatus.NOT_FOUND);
+        }
+
+        return new ResponseEntity<Participant>(participant, HttpStatus.OK);
+
+    }
 }
